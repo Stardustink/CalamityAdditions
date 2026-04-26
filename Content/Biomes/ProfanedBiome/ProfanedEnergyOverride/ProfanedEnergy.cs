e31f894b4122e6fd121ec31b9dbf7a05260a9173
InkyStarfall <forgeablecoinyt@gmail.com>
2026-04-26 17:34:18 -0400

    The Profaned Energy Override!!!1!!

---
+++ Content/Biomes/ProfanedBiome/ProfanedEnergyOverride/ProfanedEnergy.cs
@@ -0,0 +1,37 @@
+﻿using CalamityAdditions.Core.NpcBehaviorOverride;
+using CalamityMod.NPCs.NormalNPCs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalamityAdditions.Content.Biomes.ProfanedBiome.ProfanedEnergyOverride
+{
+    internal class ProfanedEnergy : NPCBehaviorOverride
+    {
+        public override int NPCType => ModContent.NPCType<ProfanedEnergyBody>();
+
+        public override void SetDefaults(NPC NPC)
+        {
+            NPC.lifeMax = 15_000;
+            NPC.noGravity = true;
+            NPC.Size = new(50);
+            NPC.Profaned().IsProfaned = true;
+        }
+
+        public override bool OverrideAI(NPC NPC)
+        {
+            return false;
+        }
+
+        public override bool PreDraw(NPC NPC, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            return false;
+        }
+
+        public override string TexturePath => this.GetPath();
+
+        public override bool OnSpawn(NPC NPC, IEntitySource source) => false;
+    }
+}
